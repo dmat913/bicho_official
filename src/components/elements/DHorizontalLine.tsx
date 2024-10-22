8100c9e9ab88d1b3940a274dc4d674756b238f53
dmat913 <soccer.82.d.y@gmail.com>
2024-10-22 21:10:36 +0900

    feat: mockデータ試合結果画面追加

--- src/components/elements/DHorizontalLine.tsx
+++ src/components/elements/DHorizontalLine.tsx
@@ -1,5 +1,5 @@
 const DHorizontalLine = () => {
-  return <hr className="border-0 h-[1px] bg-line-1" />;
+  return <hr className="border-0 h-[1px] bg-gray-400" />;
 };
 
 export default DHorizontalLine;
