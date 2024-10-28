018aab7eca5bfbaae42dea5146449e0d2677101e
dmat913 <soccer.82.d.y@gmail.com>
2024-10-28 13:58:00 +0900

    style: 試合一覧表示修正

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,9 +1,10 @@
 export const formatDate = (dateString: Date, timeString: string) => {
   const date = new Date(dateString);
+  const year = date.getFullYear();
   const month = date.getMonth() + 1;
   const day = date.getDate();
   const weekdays = ["日", "月", "火", "水", "木", "金", "土"];
   const weekday = weekdays[date.getDay()];
 
-  return `${month}/${day} (${weekday}) ${timeString}`;
+  return `${year}/${month}/${day} (${weekday}) ${timeString}`;
 };
