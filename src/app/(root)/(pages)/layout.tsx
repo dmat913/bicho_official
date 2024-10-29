47e2c7cc33ae0a53d58de6432a2fce76401c3b3d
dmat913 <soccer.82.d.y@gmail.com>
2024-10-29 11:26:20 +0900

    fix: fix build error

--- src/app/(root)/(pages)/layout.tsx
+++ src/app/(root)/(pages)/layout.tsx
@@ -4,7 +4,7 @@ import { useRouter } from "next/navigation";
 import React, { ReactNode, useEffect } from "react";
 import { useRecoilValue } from "recoil";
 
-const layout = ({ children }: { children: ReactNode }) => {
+const Layout = ({ children }: { children: ReactNode }) => {
   const router = useRouter();
   const images = useRecoilValue(imagesState);
 
@@ -17,4 +17,4 @@ const layout = ({ children }: { children: ReactNode }) => {
   return <div className="w-full h-full">{images.length > 0 && children}</div>;
 };
 
-export default layout;
+export default Layout;
