81a6ac49bf501acf64cf994bb06af21a903eb07d
dmat913 <soccer.82.d.y@gmail.com>
2024-10-29 11:07:07 +0900

    style: loadingアニメーション追加

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,55 +1,79 @@
 "use client";
-import { useEffect, useState } from "react";
-import { fetchImages } from "@/utils/image";
-import { useSetRecoilState } from "recoil";
+import { useState } from "react";
+import { useRecoilValue } from "recoil";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { Autoplay, Pagination } from "swiper/modules";
+import { motion, AnimatePresence } from "framer-motion";
+import Header from "@/components/header/Header";
+// import NavBar from "@/components/nav/NavBar";
 import { imagesState } from "@/recoil/atom/image";
-import { useRouter } from "next/navigation";
-import BichoLogo from "@/public/bicho-icon.png";
-import Image from "next/image";
+import VideoList from "@/components/pages/home/VideoList";
+import Footer from "@/components/footer/Footer";
+import DPagination from "@/components/elements/DPagination";
+import GameSchedule from "@/components/pages/home/GameSchedule";
+import HomeLoading from "@/components/pages/home/HomeLoading";
 
-const ImageUpload: React.FC = () => {
-  // 画像一覧 data
-  const setImages = useSetRecoilState(imagesState);
-  // ローディング状態
-  const [isLoading, setIsLoading] = useState(true);
-  // ルーター
-  const router = useRouter();
-
-  // 画像取得API呼び出し
-  useEffect(() => {
-    const loadImages = async () => {
-      try {
-        const data = await fetchImages();
-        setImages(data);
-        setIsLoading(false);
-      } catch (error) {
-        alert("画像取得中にエラーが発生しました:");
-        console.log(error);
-        setIsLoading(false);
-      }
-    };
-    loadImages();
-    // eslint-disable-next-line
-  }, []);
-
-  // 画像取得が完了したら /home に遷移
-  useEffect(() => {
-    if (!isLoading) {
-      router.push("/home");
-    }
-  }, [isLoading, router]);
+const HomePage: React.FC = () => {
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const images = useRecoilValue(imagesState);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   return (
-    <div className="w-[100vw] h-[100svh] bg-noise-green-3 flex items-center justify-center">
-      <Image
-        src={BichoLogo}
-        alt=""
-        height={80}
-        width={80}
-        className="animate-bounceSlow"
-      />
+    <div
+      className={`relative overflow-hidden ${
+        isLoading ? "w-[100vw] h-[100svh]" : "h-full w-full"
+      }`}
+    >
+      {/* メインコンテンツ */}
+      <div className="w-full h-full">
+        <Header />
+        {/* <NavBar /> */}
+        <Swiper
+          spaceBetween={1}
+          slidesPerView={1}
+          modules={[Autoplay, Pagination]}
+          autoplay={{ delay: 4000 }}
+          pagination={{ clickable: true }}
+          loop={true}
+          onSlideChange={(swiper) => setCurrentPage(swiper.realIndex + 1)}
+        >
+          {images.map((image) => (
+            <SwiperSlide key={image._id} className="md:h-[200px]">
+              <img
+                src={image.data}
+                alt="Top Image"
+                className="lg:h-[500px] md:h-[400px] h-[250px] object-cover w-full"
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <div className="flex gap-1 bg-noise-green-1 justify-center py-3">
+          <DPagination data={images} currentPage={currentPage - 1} />
+        </div>
+        {/** 試合日程 */}
+        <GameSchedule />
+        {/** Youtube */}
+        <VideoList />
+        <Footer />
+      </div>
+
+      {/* ローディング画面 */}
+      <AnimatePresence>
+        {isLoading && (
+          <motion.div
+            className="absolute inset-0 z-10 bg-noise-green-3 flex items-center justify-center"
+            initial={{ opacity: 1 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0, y: "-100%" }}
+            transition={{ duration: 0.8 }}
+          >
+            <HomeLoading setIsLoading={setIsLoading} />
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
 
-export default ImageUpload;
+export default HomePage;
