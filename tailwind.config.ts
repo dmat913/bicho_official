2c65d26bac7fca3aa26b703d79cccde5e6dbb1d5
dmat913 <soccer.82.d.y@gmail.com>
2024-11-05 00:24:05 +0900

    style: トーナメントアニメーション追加

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -29,9 +29,13 @@ const config: Config = {
         text: {
           1: "#333333",
         },
+        gray: {
+          1: "#e5e7eb",
+        },
       },
       animation: {
         bounceSlow: "bounceSlow 1s infinite",
+        // movePattern: "movePattern 8s ease-in-out forwards",
       },
       keyframes: {
         bounceSlow: {
@@ -44,6 +48,14 @@ const config: Config = {
             animationTimingFunction: "cubic-bezier(0, 0, 0.2, 1)",
           },
         },
+        // movePattern: {
+        //   "0%": { transform: "translate(0, 0)" },
+        //   "20%": { transform: "translate(120px, 0)" },
+        //   "40%": { transform: "translate(120px, 20px)" },
+        //   "60%": { transform: "translate(170px, 20px)" },
+        //   "80%": { transform: "translate(170px, -14px)" },
+        //   "100%": { transform: "translate(210px, -14px)" },
+        // },
       },
       backgroundImage: {
         "custom-gradient":
