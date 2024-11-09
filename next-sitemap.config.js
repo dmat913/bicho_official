33dd7aac5ae370952b5950540b329f6cb50e2f78
dmat913 <soccer.82.d.y@gmail.com>
2024-11-09 22:47:11 +0900

    chore: sitemap追加

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+// next-sitemap.config.js
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: "https://bicho-official.vercel.app",
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+};
+
+module.exports = config;
