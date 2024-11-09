4af5e11e507d89bed8de51d666106751c13ecdaf
dmat913 <soccer.82.d.y@gmail.com>
2024-11-09 22:40:31 +0900

    feat: SEO対策

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,25 @@
+// app/layout.tsx
 import type { Metadata } from "next";
 import "./globals.css";
 
+// メタデータの設定
 export const metadata: Metadata = {
   title: "Bicho Official",
   description: "Bicho",
+  keywords: ["Bicho", "official", "website"],
+  openGraph: {
+    title: "Bicho Official",
+    description: "Welcome to the official Bicho site.",
+    url: "https://bicho-official.vercel.app",
+    siteName: "Bicho Official",
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Bicho Official",
+    description: "Bicho's official site on the web.",
+  },
 };
 
 export default function RootLayout({
@@ -13,6 +29,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head />
       <body>{children}</body>
     </html>
   );
