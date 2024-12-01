9522fc61b114216868143c64161516198341a475
dmat913 <soccer.82.d.y@gmail.com>
2024-12-01 21:33:27 +0900

    fix: フォーメーションフォルダ作成

---
+++ src/data/formation/4-3-3.ts
@@ -0,0 +1,110 @@
+import YUUTOKAGAWA_IMG from "@/public/profile/YUUTOKAGAWA.png";
+import SATHUKITAKISHIMA_IMG from "@/public/profile/SATHUKITAKISHIMA.png";
+import AYUMUENOMOTO_IMG from "@/public/profile/AYUMUENOMOTO.png";
+import HIKARUIWASE_IMG from "@/public/profile/HIKARUIWASE.png";
+import DAIKIYAMAGUCHI_IMG from "@/public/profile/DAIKIYAMAGUCHI.png";
+import RYOUISHIKAWA_IMG from "@/public/profile/RYOUISHIKAWA.png";
+import YUDAINAKATA_IMG from "@/public/profile/YUDAINAKATA.png";
+import GOUKOIDA_IMG from "@/public/profile/GOUKOIDA.png";
+import TAIGAASAKO_IMG from "@/public/profile/TAIGAASAKO.png";
+import YASUTOISHIKAWA_IMG from "@/public/profile/YASUTOISHIKAWA.png";
+import RIKUOONO_IMG from "@/public/profile/RIKUOONO.png";
+
+/** 4-3-3データ */
+export const FOUR_THREE_THREE = [
+  {
+    name: "SATHUKI",
+    number: 28,
+    position: "GK",
+    imageUrl: SATHUKITAKISHIMA_IMG,
+    styles: {
+      left: "50%",
+      transform: "translateX(-50%)",
+      top: "14%",
+      width: "48px",
+      height: "66px",
+    },
+  },
+  {
+    name: "KAGAWA",
+    number: 4,
+    position: "CB",
+    imageUrl: YUUTOKAGAWA_IMG,
+    styles: { left: "26%", top: "20%", width: "56px", height: "76px" },
+  },
+  {
+    name: "小飯田豪",
+    number: 5,
+    position: "CB",
+    imageUrl: GOUKOIDA_IMG,
+    styles: { right: "26%", top: "20%", width: "56px", height: "76px" },
+  },
+  {
+    name: "石川靖人",
+    number: 2,
+    position: "RB",
+    imageUrl: YASUTOISHIKAWA_IMG,
+    styles: { left: "4%", top: "24%", width: "58px", height: "80px" },
+  },
+  {
+    name: "大野莉久",
+    number: 26,
+    position: "RB",
+    imageUrl: RIKUOONO_IMG,
+    styles: { right: "4%", top: "24%", width: "58px", height: "80px" },
+  },
+  {
+    name: "浅子太我",
+    number: 41,
+    position: "CDM",
+    imageUrl: TAIGAASAKO_IMG,
+    styles: {
+      left: "50%",
+      transform: "translateX(-50%)",
+      top: "36%",
+      width: "58px",
+      height: "82px",
+    },
+  },
+  {
+    name: "中田湧大",
+    number: 8,
+    position: "RCM",
+    imageUrl: YUDAINAKATA_IMG,
+    styles: { left: "23%", top: "44%", width: "60px", height: "86px" },
+  },
+  {
+    name: "山口大貴",
+    number: 17,
+    position: "LCM",
+    imageUrl: DAIKIYAMAGUCHI_IMG,
+    styles: { right: "23%", top: "44%", width: "60px", height: "86px" },
+  },
+  {
+    name: "岩瀬輝",
+    number: 19,
+    position: "LW",
+    imageUrl: HIKARUIWASE_IMG,
+    styles: { left: "2%", top: "60%", width: "66px", height: "94px" },
+  },
+  {
+    name: "石川諒",
+    number: 18,
+    position: "RW",
+    imageUrl: RYOUISHIKAWA_IMG,
+    styles: { right: "2%", top: "60%", width: "66px", height: "94px" },
+  },
+  {
+    name: "榎本歩夢",
+    number: 11,
+    position: "CF",
+    imageUrl: AYUMUENOMOTO_IMG,
+    styles: {
+      left: "50%",
+      transform: "translateX(-50%)",
+      top: "66%",
+      width: "70px",
+      height: "100px",
+    },
+  },
+];
--- src/features/home/starting-lineup/SoccerGround.tsx
+++ src/features/home/starting-lineup/SoccerGround.tsx
@@ -3,117 +3,10 @@ import React, { memo, useRef } from "react";
 import styles from "./SoccerGround.module.css";
 import { motion, useInView } from "framer-motion";
 import PlayerCard from "./PlayerCard";
-import YUUTOKAGAWA_IMG from "@/public/profile/YUUTOKAGAWA.png";
-import SATHUKITAKISHIMA_IMG from "@/public/profile/SATHUKITAKISHIMA.png";
-import AYUMUENOMOTO_IMG from "@/public/profile/AYUMUENOMOTO.png";
-import HIKARUIWASE_IMG from "@/public/profile/HIKARUIWASE.png";
-import DAIKIYAMAGUCHI_IMG from "@/public/profile/DAIKIYAMAGUCHI.png";
-import RYOUISHIKAWA_IMG from "@/public/profile/RYOUISHIKAWA.png";
-import YUDAINAKATA_IMG from "@/public/profile/YUDAINAKATA.png";
-import GOUKOIDA_IMG from "@/public/profile/GOUKOIDA.png";
-import TAIGAASAKO_IMG from "@/public/profile/TAIGAASAKO.png";
-import YASUTOISHIKAWA_IMG from "@/public/profile/YASUTOISHIKAWA.png";
-import RIKUOONO_IMG from "@/public/profile/RIKUOONO.png";
 import BackgroundGround from "@/public/backgroundGround.jpg";
 import Image from "next/image";
-
-const players = [
-  {
-    name: "SATHUKI",
-    number: 28,
-    position: "GK",
-    imageUrl: SATHUKITAKISHIMA_IMG,
-    styles: {
-      left: "50%",
-      transform: "translateX(-50%)",
-      top: "14%",
-      width: "48px",
-      height: "66px",
-    },
-  },
-  {
-    name: "KAGAWA",
-    number: 4,
-    position: "CB",
-    imageUrl: YUUTOKAGAWA_IMG,
-    styles: { left: "26%", top: "20%", width: "56px", height: "76px" },
-  },
-  {
-    name: "小飯田豪",
-    number: 5,
-    position: "CB",
-    imageUrl: GOUKOIDA_IMG,
-    styles: { right: "26%", top: "20%", width: "56px", height: "76px" },
-  },
-  {
-    name: "石川靖人",
-    number: 2,
-    position: "RB",
-    imageUrl: YASUTOISHIKAWA_IMG,
-    styles: { left: "4%", top: "24%", width: "58px", height: "80px" },
-  },
-  {
-    name: "大野莉久",
-    number: 26,
-    position: "RB",
-    imageUrl: RIKUOONO_IMG,
-    styles: { right: "4%", top: "24%", width: "58px", height: "80px" },
-  },
-  {
-    name: "浅子太我",
-    number: 41,
-    position: "CDM",
-    imageUrl: TAIGAASAKO_IMG,
-    styles: {
-      left: "50%",
-      transform: "translateX(-50%)",
-      top: "36%",
-      width: "58px",
-      height: "82px",
-    },
-  },
-  {
-    name: "中田湧大",
-    number: 8,
-    position: "RCM",
-    imageUrl: YUDAINAKATA_IMG,
-    styles: { left: "23%", top: "44%", width: "60px", height: "86px" },
-  },
-  {
-    name: "山口大貴",
-    number: 17,
-    position: "LCM",
-    imageUrl: DAIKIYAMAGUCHI_IMG,
-    styles: { right: "23%", top: "44%", width: "60px", height: "86px" },
-  },
-  {
-    name: "岩瀬輝",
-    number: 19,
-    position: "LW",
-    imageUrl: HIKARUIWASE_IMG,
-    styles: { left: "2%", top: "60%", width: "66px", height: "94px" },
-  },
-  {
-    name: "石川諒",
-    number: 18,
-    position: "RW",
-    imageUrl: RYOUISHIKAWA_IMG,
-    styles: { right: "2%", top: "60%", width: "66px", height: "94px" },
-  },
-  {
-    name: "榎本歩夢",
-    number: 11,
-    position: "CF",
-    imageUrl: AYUMUENOMOTO_IMG,
-    styles: {
-      left: "50%",
-      transform: "translateX(-50%)",
-      top: "66%",
-      width: "70px",
-      height: "100px",
-    },
-  },
-];
+// import { FOUR_THREE_THREE } from "@/data/formation/4-3-3";
+import { FOUR_THREE_ONE_TWO } from "@/data/formation/4-3-1-2";
 
 const SoccerGround = () => {
   const ref = useRef(null);
@@ -156,7 +49,7 @@ const SoccerGround = () => {
           <div className={styles.centerCircle}></div>
         </div>
 
-        {players.map((player, index) => (
+        {FOUR_THREE_ONE_TWO.map((player, index) => (
           <motion.div
             key={index}
             initial={{ opacity: 0, y: 50 }}
