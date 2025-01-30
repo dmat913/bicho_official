9a90e252a19c2f8100a1d5411f7680bdd1f5ea32
dmat913 <soccer.82.d.y@gmail.com>
2025-01-30 23:02:03 +0900

    feat: 活動比率データ追加

--- src/features/data/data/doughnutData.ts
+++ src/features/data/data/doughnutData.ts
@@ -1,3 +1,6 @@
+import { ChartOptions } from "chart.js";
+import { Context } from "chartjs-plugin-datalabels";
+
 export const AgeData = {
   labels: ["20代", "30代", "40代"],
   datasets: [
@@ -13,10 +16,55 @@ export const AgeData = {
   ],
 };
 
-export const AgeOptions = {
+export const AgeOptions: ChartOptions<"doughnut"> = {
   plugins: {
     legend: {
       display: false,
+    },
+    datalabels: {
+      display: false,
+    },
+  },
+};
+
+export const activeRatioData = {
+  labels: ["公式戦", "練習試合", "練習", "OFF"],
+  datasets: [
+    {
+      data: [15, 11, 18, 7],
+      backgroundColor: [
+        "rgb(255, 99, 132)",
+        "rgb(54, 162, 235)",
+        "rgb(255, 205, 86)",
+        "rgb(75, 192, 192)",
+      ],
+      borderColor: [
+        "rgb(0, 0, 132)",
+        "rgb(0, 0, 132)",
+        "rgb(0, 0, 132)",
+        "rgb(0, 0, 132)",
+      ],
+    },
+  ],
+};
+
+export const activeRatioOptions: ChartOptions<"pie"> = {
+  responsive: true,
+  plugins: {
+    legend: {
+      display: false,
+    },
+    datalabels: {
+      color: "#fff",
+      font: { size: 12, weight: "bold" },
+      formatter: (value: number, context: Context) => {
+        const label = context.chart.data.labels?.[context.dataIndex];
+
+        // ラベルと値を返す
+        return label ? `${label}:${value}` : `${value}`;
+      },
+      anchor: "center",
+      align: "center",
     },
   },
 };
