0155561220a67bc6373d0d7b6ef241de37d1e47f
daiki yamaguchi <soccer.82.d.y@gmail.com>
2026-03-11 21:45:45 +0900

    address build error

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const NotFound = () => {
   return (
     <div className="p-6 flex flex-col items-center justify-center h-screen bg-green-1 text-white-1">
@@ -6,12 +8,12 @@ const NotFound = () => {
       <p className="mt-4 text-center">
         申し訳ありませんが、入力されたURLは存在しないか、移動された可能性があります。
       </p>
-      <a
+      <Link
         href="/"
         className="mt-6 px-4 py-2 bg-green-2 text-white-2 rounded hover:bg-green-3 transition duration-300"
       >
         ホームに戻る
-      </a>
+      </Link>
     </div>
   );
 };
